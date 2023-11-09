e104c6e7cd6303665bc1d66adfa8c15bd16e8f85
Andres Roa <andresmore1423@hotmail.com>
2023-11-09 15:47:29 -0500

    Modified findOne by slug api

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
+    async findeOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.service('api::blog.blog').findeOne({
+            where: { slug: id }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity)
+    }
+}));
