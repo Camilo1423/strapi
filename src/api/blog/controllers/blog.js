4c73bb0ee16425569a9bca4ae6efa8c60c9a3970
Andres Roa <andresmore1423@hotmail.com>
2023-11-09 16:01:46 -0500

    another try

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -7,14 +7,26 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
-    async findeOne(ctx) {
-        const { id } = ctx.params;
+    async findOne(ctx) {
+        // Obtener el slug desde los parámetros de la consulta
+        const { slug } = ctx.params;
 
-        const entity = await strapi.service('api::blog.blog').findeOne({
-            where: { slug: id }
+        // Utilizar findMany con un filtro para buscar por slug
+        const entities = await strapi.entityService.findMany('api::blog.blog', {
+            filters: { slug },
+            ...this.getFetchParams(ctx.query),
         });
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-        return this.transformResponse(sanitizedEntity)
+        // Manejar el caso de no encontrar resultados o encontrar más de uno
+        if (entities.length === 0) {
+            return ctx.notFound('No se encontró el blog con ese slug.');
+        }
+
+        if (entities.length > 1) {
+            return ctx.badRequest('Múltiples blogs encontrados con ese slug, por favor sea más específico.');
+        }
+
+        // Devolver el primer elemento ya que debería haber solo uno
+        return this.sanitizeOutput(entities[0], ctx);
     }
 }));
